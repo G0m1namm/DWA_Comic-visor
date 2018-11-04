aa87ae4aae0fac2c5fa6d2f387037cd723c387c8
Jherson Martelo <yesungpapu@gmail.com>
2018-11-03 21:15:34 -0500

    sistema de likes terminado, ahora falta la funcionalidad de busqueda de comics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { ComicService } from './services/comics.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListCardsComponent
+    ListCardsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
